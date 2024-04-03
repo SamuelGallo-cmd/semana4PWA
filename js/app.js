f5418ac18bb071b7a528f5733f93f592c7c44973
SamuelGallo-cmd <Samuelgallogaitan@gmail.com>
2024-04-03 17:59:15 -0600

    service worker

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
+let url=window.location.ref;
+let swLoc="unatask/service-worker.js";
+
 if(navigator.serviceWorker){
+
+if(url.includes('localhost')){
+    swLoc='/service-worker.js';
+}
+
+
     navigator.serviceWorker.register('/service-worker.js')
         .then(reg=>{
             // setTimeout(()=>{
